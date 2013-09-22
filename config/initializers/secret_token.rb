748aa69da0facae52ae6fca79ca175f303ce7cd5
little-hands <majackyy@gmail.com>
2013-09-23 08:01:24 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tatekaeapp::Application.config.secret_token = 'b9ec9588c9552b27244a34b8c9c160e69301050c06b6e0ffaea4578d9e89fd55d420146bb1e7d7ff2c92af2af4d104e747c282c2ed269db7c6a370724b8ae5c3'
