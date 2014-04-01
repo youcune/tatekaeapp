fe3a9f64240986b19a79a3dd90599b28f79422d0
little-hands <majackyy@gmail.com>
2014-04-01 14:16:56 +0900

    イベント一覧 降順に変更

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
 	http_basic_authenticate_with :name => "jack", :password => "jack"  
 
   def events_index
-    @events = Event.find(:all,order:"created_at")
+    @events = Event.find(:all,order:"created_at desc")
   end
 end
