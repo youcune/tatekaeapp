02315ff26c14293ce17b15d199a3c76c7e990dc2
little-hands <majackyy@gmail.com>
2014-04-02 13:14:44 +0900

    paymentとexemptionをトランザクション処理

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -43,44 +43,45 @@ class PaymentsController < ApplicationController
     @payment = Payment.find(params[:id])
     @paticipants = @payment.event.paticipants
 
-    #paitcipantごとに処理を行う
-    @paticipants.each do |paticipant|
-      #処理用に指定payment、paticipantごとのexemptionを検索する
-      exemption = Exemption.find(:first,
-          :conditions =>["paticipant_id=? and payment_id=?",paticipant.id,@payment.id])
+    ActiveRecord::Base.transaction do
 
-      #exemptionチェックが1個も無いとparams[:check_payment] がnilになり
-      #params[:check_payment][:"#{paticipant.id}"]が取得できないので、その場合はｎｉｌを設定する
-      if params[:check_payment] == nil
-        param_check =nil
-      else
-        param_check = params[:check_payment][:"#{paticipant.id}"]
-      end
+      #paitcipantごとに処理を行う
+        @paticipants.each do |paticipant|
+        #処理用に指定payment、paticipantごとのexemptionを検索する
+        exemption = Exemption.find(:first,
+            :conditions =>["paticipant_id=? and payment_id=?",paticipant.id,@payment.id])
 
-      if param_check == nil
-        #チェックされていない場合(=支払を行なわない・免除(exemption)がある人)：データがある状態が正しい
-        #既存のexemptionデータがない場合、データを登録する
-        if exemption ==nil 
-          exemption  = Exemption.new(paticipant_id:paticipant.id,payment_id:@payment.id)
-          exemption.save
-        end  
-        #既存のexemptionデータがある場合、何も行わない
-      else
-        #チェックされている(支払を行う人)場合：データがない状態が正しい
-        #既存のexemptionデータがある場合、exemptionを削除する
-        if exemption !=nil 
-          exemption.destroy
-        end  
-        #既存のexemptionデータがない場合、何も行わない
+        #exemptionチェックが1個も無いとparams[:check_payment] がnilになり
+        #params[:check_payment][:"#{paticipant.id}"]が取得できないので、その場合はｎｉｌを設定する
+        if params[:check_payment] == nil
+          param_check =nil
+        else
+          param_check = params[:check_payment][:"#{paticipant.id}"]
+        end
+
+        if param_check == nil
+          #チェックされていない場合(=支払を行なわない・免除(exemption)がある人)：データがある状態が正しい
+          #既存のexemptionデータがない場合、データを登録する
+          if exemption ==nil 
+            exemption  = Exemption.new(paticipant_id:paticipant.id,payment_id:@payment.id)
+            exemption.save!
+          end  
+          #既存のexemptionデータがある場合、何も行わない
+        else
+          #チェックされている(支払を行う人)場合：データがない状態が正しい
+          #既存のexemptionデータがある場合、exemptionを削除する
+          if exemption !=nil 
+            exemption.destroy!
+          end  
+          #既存のexemptionデータがない場合、何も行わない
+        end
       end
+      @payment.update_attributes!(params[:payment])
     end
-
-    if @payment.update_attributes(params[:payment])
       flash[:notice] = "支払項目が更新されました。"
       redirect_to :controller => 'events',:action =>'show',:id=>@payment.event.str_id
-    else
+    rescue
       render action: "edit" 
-    end
   end
 
   # DELETE /payments/1
